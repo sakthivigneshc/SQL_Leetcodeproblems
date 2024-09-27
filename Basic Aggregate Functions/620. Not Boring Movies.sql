986e4cc21d53604c66075d6e9168e81120493f65
C <sakthivigneshc3@gmail.com>
2024-09-27 11:40:01 +0530

    620. Not Boring Movies.sql soln

--- Basic Aggregate Functions/620. Not Boring Movies.sql
+++ Basic Aggregate Functions/620. Not Boring Movies.sql
@@ -1,3 +1,13 @@
 SELECT id, movie, description,rating 
 FROM Cinema
 WHERE   description != "boring"  AND  id %2 =1  ORDER BY rating DESC ;
+
+
+/*
+
+SELECT id, movie,description,rating 
+FROM Cinema
+WHERE NOT description="boring" AND id%2!=0
+ORDER BY rating DESC;
+
+*/
